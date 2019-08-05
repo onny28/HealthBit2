dafa395550c2862c81d4bd2f4d1aea2db5108160
Rabi'atul <nws06_01094262@pb.edu.bn>
2019-08-05 17:44:16 +0800

    create homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },  { path: 'homepage', loadChildren: './homepage/homepage.module#HomepagePageModule' },
+
 ];
 
 @NgModule({
