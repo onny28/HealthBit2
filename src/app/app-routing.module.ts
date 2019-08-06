329be0ae236f8f384a9a707494920e401edde398
Rabi'atul <nws06_01094262@pb.edu.bn>
2019-08-06 09:41:03 +0800

    generate pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },  { path: 'record', loadChildren: './record/record.module#RecordPageModule' },
+  { path: 'grocerylist', loadChildren: './grocerylist/grocerylist.module#GrocerylistPageModule' },
+  { path: 'setting', loadChildren: './setting/setting.module#SettingPageModule' },
+
 ];
 
 @NgModule({
