2acd689fb0f8d0675b74ef04c8adf4a93b0c5cf0
Rabi'atul <nws06_01094262@pb.edu.bn>
2019-08-06 14:09:44 +0800

    creating recipe page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },  { path: 'record', loadChildren: './record/record.module#RecordPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'record', loadChildren: './record/record.module#RecordPageModule' },
   { path: 'grocerylist', loadChildren: './grocerylist/grocerylist.module#GrocerylistPageModule' },
-  { path: 'setting', loadChildren: './setting/setting.module#SettingPageModule' },
+  { path: 'setting', loadChildren: './setting/setting.module#SettingPageModule' },  { path: 'recipe', loadChildren: './recipe/recipe.module#RecipePageModule' },
 
 ];
 
