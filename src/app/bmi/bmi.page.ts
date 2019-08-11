a9735d4e8a313567d42bce85eb96e4637fffcf53
onny <nws06_01090502@pb.edu.bn>
2019-08-12 00:02:42 +0800

    ubah

---
+++ src/app/bmi/bmi.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bmi',
+  templateUrl: './bmi.page.html',
+  styleUrls: ['./bmi.page.scss'],
+})
+export class BmiPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
