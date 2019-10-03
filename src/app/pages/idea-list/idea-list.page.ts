c30fa97bf4d71d09ace9fe873e8d74a498d27adc
Rabi'atul <nws06_01094262@pb.edu.bn>
2019-10-03 18:23:58 +0800

    share button

--- src/app/pages/idea-list/idea-list.page.ts
+++ src/app/pages/idea-list/idea-list.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IdeaService, Idea } from '../../services/idea.service';
 import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore'
 
 @Component({
   selector: 'app-idea-list',
@@ -9,11 +10,41 @@ import { Observable } from 'rxjs';
 })
 export class IdeaListPage implements OnInit {
 
-  private ideas: Observable<Idea[]>;
- 
-  constructor(private ideaService: IdeaService) { }
+private ideas: Observable<Idea[]>;
+ public ideaList: any[];
+ private loadedIdeaList: any[];
+  constructor(private ideaService: IdeaService, private firestore:AngularFirestore) { }
  
   ngOnInit() {
     this.ideas = this.ideaService.getIdeas();
+     
+  //   this.firestore.collection('recipe').valueChanges()
+  //   .subscribe(ideaList => {
+  //     this.ideaList = ideaList;
+  //     this.loadedIdeaList = ideaList;
+  // });
   }
+
+  // initializeItems(): void {
+  //   this.ideaList = this.loadedIdeaList;
+  // }
+
+//   filterList(evt) {
+    
+  
+//     const searchTerm = evt.srcElement.value;
+  
+//     if (!searchTerm) {
+//       return;
+//     }
+  
+//     this.ideas= this.ideas.filter(idea => {
+//       if (idea.name && searchTerm) {
+//         if (idea.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+//           return true;
+//         }
+//         return false;
+//       }
+//     });
+// }
 }
